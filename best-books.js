0d1cff491a5fe38735a5ebd1d8efd6de0a1b6c50
Reetika <chauhanreetika45@gmail.com>
2023-12-04 17:14:14 -0800

    implemented all questions except credit question

--- best-books.js
+++ best-books.js
@@ -1,14 +1,32 @@
 const formEl = document.getElementById('best-books-form');
-const yearEl = document.getElementById('year');
-const monthEl = document.getElementById('month');
-const dateEl = document.getElementById('date');
-
+const books_API = 'MzLSqkQGr1AXd5AqVsu3zs9bUngM5FDg'
+BASE_URL = `https://api.nytimes.com/svc/books/v3/lists/`
 formEl.addEventListener('submit', function(e) {
   e.preventDefault();
-
-  const year = yearEl.value;
-  const month = monthEl.value;
-  const date = dateEl.value;
-
+  const year = document.getElementById('year').value;
+  const month = document.getElementById('month').value;
+  const date = document.getElementById('date').value;
+  
+  const books_date = `${year}-${ month}-${date}`
   // Fetch bestselling books for date and add top 5 to page
+  fetch(`${BASE_URL}${books_date}/hardcover-fiction.json?api-key=${books_API}`)
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(responseJson) {
+    const top5books = responseJson.results.books.slice(0,5);
+    console.log(top5books)
+    top5books.forEach(book =>{
+        const bookone = `Title: ${book.title}, Author: ${book.author}, Description: ${book.description}`
+        console.log(bookone)
+        const display = document.getElementById("books-container")
+        let pele = document.createElement("p")
+        pele.innerHTML = bookone
+        display.appendChild(pele)
+    })
+  })
+  .catch(function (responseJson){
+     console.log("Api didn't work for", responseJson)
+  })
+
 });
